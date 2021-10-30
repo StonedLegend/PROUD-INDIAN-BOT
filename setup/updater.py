9d8103800c194475d1b72e59e3950e5561c406ee
STONED LEGEND AGORA <93366460+StonedLegend@users.noreply.github.com>
2021-10-30 20:44:14 +0530

    Update updater.py

--- setup/updater.py
+++ setup/updater.py
@@ -42,7 +42,7 @@ async def update_requirements(main , test):
     try:
         for i in a:
             await runcmd(f"pip install {i}")
-            print(f"🇮🇳ᴾᴿᴼᵁᴰ𝗜𝗡𝗗𝗜𝗔𝗡 𝗕𝗢𝗧 V3.0🇮🇳 {i}")
+            print(f"PROUD-⚡🇮🇳⚡INDIAN-BOT⚡🇮🇳⚡ {i}")
     except Exception as e:
         print(f"Error while installing requirments {str(e)}")
 
